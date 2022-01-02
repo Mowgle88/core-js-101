23285ebe3f20c963a75865b3e5b6fa4e6789d9d4
Mowgle88 <mowgle1988@gmail.com>
2022-01-03 00:51:47 +0300

    feat: resolve 29 problems

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -191,8 +191,9 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  // throw new Error('Not implemented');
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -212,8 +213,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  // throw new Error('Not implemented');
+  for (let i = 2; i <= n / 2; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -231,8 +238,15 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  // throw new Error('Not implemented');
+  if (typeof +value === 'number' || value instanceof Number) {
+    if (Number.isNaN(+value)) {
+      return def;
+    }
+    return +value;
+  }
+  return def;
 }
 
 module.exports = {
